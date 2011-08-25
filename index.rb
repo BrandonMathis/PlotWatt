988e51a8b1b12da52e5b8d7b447a7f957ef52db1
BrandonMathis <Brandon@KeysetTS.com>
2011-08-25 17:08:38 -0400

    Now with awesome google pie and line charts

--- index.rb
+++ index.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'mongo'
 require 'mongoid'
 require 'haml'
+require 'awesome_print'
+require 'gchart'
+
 
 Dir.glob("lib/**").each do |file|
   require File.join(File.dirname(__FILE__), file)
@@ -16,8 +19,27 @@ configure do
   end
 end
 
-get '/' do
+get '/raw_data' do
   @fields = Measurment.field_names
   @measurments = Measurment.find(:all)
+  haml :raw_data
+end
+
+get '/' do
+  measurments = Measurment.find(:all)
+  total_over_time = measurments.map { |meas| meas.total_usage }
+  labels = measurments.map { |meas| meas.collected.to_s }
+  @line=Gchart.line(:size => '900x300',
+            :title => "Energy Usage",
+            :bg => 'efefef',
+            :legend => ['Total Usage'],
+            :data => total_over_time,
+            )
+  @pie=Gchart.pie_3d(
+    :size => '900x300',
+    :title => "Total Usage",
+    :data => Measurment.totals,
+    :labels => ["Always on", "Heating & A/C", "Refrigeration", "Dryer", "Cooking", "Other"]
+  )
   haml :index
 end
--- lib/measurment.rb
+++ lib/measurment.rb
@@ -11,4 +11,19 @@ class Measurment
   def self.field_names
     fields.reject { |name, field| name =~ /_id(s?)$/ || name == "_type" }.collect { |name, field| name.to_sym }
   end
+
+  def self.totals
+    totals = {:always_on => 0, :ac => 0, :refrigeration => 0, :dryer => 0, :cooking => 0, :other => 0}
+    measurments = find(:all)
+    measurments.each do |meas|
+      totals.each do |key, value|
+        totals[key] = totals[key] + meas.send(key)
+      end
+    end
+    totals.values
+  end
+
+  def total_usage
+    self.always_on + self.ac + self.refrigeration + self.dryer + self.cooking + self.other
+  end
 end
