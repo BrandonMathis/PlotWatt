ea41f179797ef2bdf51bdc1172756d5dfbc73aa5
BrandonMathis <Brandon@KeysetTS.com>
2011-08-26 12:35:04 -0400

    Added sweet labels

--- index.rb
+++ index.rb
@@ -13,7 +13,7 @@ end
 MONTHS = [nil, "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
 configure do
-   Mongoid.configure do |config|
+  Mongoid.configure do |config|
     name = "PlotWatt"
     host = "localhost"
     config.master = Mongo::Connection.new.db(name)
@@ -29,7 +29,7 @@ end
 
 get '/' do
   measurments = Measurment.find(:all)
-  @line = line_chart(measurments)
+  @line = line_chart(measurments, :range => 'lifetime')
   @pie = total_pie_chart(Measurment.totals)
   monthly_measurments = {}
   MONTHS.each do |month|
@@ -43,7 +43,7 @@ get '/energy/:month' do
   if measurments.empty?
     haml :no_usage
   elsif MONTHS.include?(params[:month])
-    @line = line_chart(measurments)
+    @line = line_chart(measurments, :range => params[:month])
     @pie = total_pie_chart(Measurment.totals_for_month(params[:month]))
     haml :month
   end
@@ -58,13 +58,28 @@ def total_pie_chart(totals)
   )
 end
 
-def line_chart(measurments)
+def line_chart(measurments, opts = {})
   totals_over_time = measurments.map { |meas| meas.total_usage }
+  # These axis lables may be a bit off but it is the best I can do for a prototype
+  axis = ( MONTHS[9,12] + MONTHS[1,3] ).join('|') if opts[:range] == 'lifetime'
+  axis = days_for_month opts[:range] if MONTHS.include?(opts[:range])
   Gchart.line(
     :size => '900x300',
     :title => "Energy Usage",
     :bg => 'efefef',
     :legend => ['Total Usage'],
     :data => totals_over_time,
+    :axis_with_labels => 'x',
+    :axis_labels => [axis]
   )
+end
+
+def days_for_month(month)
+  if ["Sept", "April", "June", "Nov"].include?(month)
+    return (1..30).to_a.join("|")
+  elsif month == "Feb" 
+    return (1..28).to_a.join("|")
+  else
+    return (1..31).to_a.join("|")
+  end
 end
